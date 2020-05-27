23d8c576301049d1d23c49780dce48ae8b6aa6fb
45022338 <296888680@qq.com>
2020-05-27 15:29:01 +0800

    add dataFrame demo

---
+++ src/main/python/dataFrame/dataFrame.py
@@ -0,0 +1,14 @@
+from pyspark.sql import SparkSession
+
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName(
+        "wordCount").master("local").getOrCreate()
+    users = spark.read.json("./src/main/python/dataFrame/user.json")
+    users.show()
+    users.printSchema()
+    users.select("name").show()
+    users.select(users["name"], users["age"]+1).show()
+    users.filter(users["age"] == 10).show()
+    users.filter(users["age"] > 10).show()
+    users.groupBy("age").count().show()
